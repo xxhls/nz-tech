0ecbd32aedf4256bd9d91d1dcfa5d2a1cc5fc353
xxhls <xxhls@vip.qq.com>
2025-01-01 12:54:26 +0800

    feat: 添加插件demo

--- docs/rspress.config.ts
+++ docs/rspress.config.ts
@@ -1,10 +1,10 @@
 import * as path from 'node:path';
-import { defineConfig } from 'rspress/config';
 import { pluginNodePolyfill } from '@rsbuild/plugin-node-polyfill';
+import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   base: '/nz-tech/',
-  root: path.join(__dirname,),
+  root: path.join(__dirname),
   title: 'My Site',
   icon: '/rspress-icon.png',
   logo: {
@@ -20,5 +20,5 @@ export default defineConfig({
       },
     ],
   },
-  builderPlugins: [pluginNodePolyfill()]
+  builderPlugins: [pluginNodePolyfill()],
 });
